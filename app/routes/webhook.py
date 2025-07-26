bfff36d6b23c00ad1379b869d92ec324c669e8db
jestruyo <jesusdavidtrujilloteheran@gmail.com>
2025-07-26 17:50:50 -0500

    Se crea diccionario de mensajes

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from ..extensions import twilio_client
-from ..state import user_state
+from ..state import user_state, output_mesage
 from ..utils import validate_user_number, TWILIO_PHONE
 import time
 
@@ -14,19 +14,7 @@ def webhook():
 
     if not user_data:
         twilio_client.messages.create(
-            body='❌ 😔 *Lo siento, no estás registrado para usar este servicio.*\
-                \n\nSi deseas registrarte, por favor contacta a tu super de grupo.\
-                \n\nMientras tanto, quiero compartirte los horarios de reunión de la congregacion cordialidad:\
-                \n\n🖥️ *1. Reunión entre semana | Jueves 7 pm*\
-                \n📌 https://jworg.zoom.us/j/99106008401\
-                \n*Código de acceso:* 1234.\
-                \n\n🖥️ *2. Reunión fin de semana | Domingo 5 pm*\
-                \n📌 https://jworg.zoom.us/j/91928746645\
-                \n*Código de acceso:* 1234.\
-                \n\n📍 *3. Dirección:* Cl. 76 # 7B - 45, El Bosque, Barranquilla.\
-                \n📌 https://maps.app.goo.gl/gteL23tYX32KUeb79\
-                \n\n🌐 *4. Sitio oficial:*\
-                \n📌 https://www.jw.org/es/',
+            body=output_mesage["not_registered_message"],
             from_=TWILIO_PHONE,
             to=from_number
         )
@@ -53,7 +41,7 @@ def webhook():
     else:
         if message_body not in ['menu', 'menú']:
             twilio_client.messages.create(
-                body=f'Hola {user_data["name"]} 😊 👋, por favor escribe *"menú"* para comenzar.',
+                body=output_mesage["welcome_message"].format(name=user_data['name']),
                 from_=TWILIO_PHONE,
                 to=from_number
             )
@@ -73,15 +61,7 @@ def webhook():
 
     if current_state == 'inicio':
         twilio_client.messages.create(
-            body=f'*¡Ahora sí, mi querid@ herman@ {user_data["name"]}. 😊 ¿Cómo puedo ayudarte?*\n\n'
-                 '<<Escribe el # número de tu opción requerida>>:\n\n'
-                 '🕒 *1. Horas de reuniones.*\n'
-                 '🏠 *2. Lugares de Predicación.*\n'
-                 '📝 *3. Envío de informes.*\n'
-                 '📋 *4. Solicitudes.*\n'
-                 '👌 *5. Siervos ministeriales.*\n'
-                 '🫶 *6. Ancianos.*\n'
-                 '🚨 *7. Líneas de emergencia.*\n',
+            body=output_mesage["start_message"].format(name=user_data['name']),
             from_=TWILIO_PHONE,
             to=from_number
         )
@@ -94,32 +74,14 @@ def webhook():
 
         if message_body == '1':
             twilio_client.messages.create(
-                body='*Estos son los enlaces a la plataforma Zoom:*\
-                      \n\n🖥️ *1. Reunión entre semana | Jueves 7 pm*\
-                      \n📌 https://jworg.zoom.us/j/99106008401\
-                      \n*Código de acceso:* 1234.\
-                      \n\n🖥️ *2. Reunión fin de semana | Domingo 5 pm*\
-                      \n📌 https://jworg.zoom.us/j/91928746645\
-                      \n*Código de acceso:* 1234.\
-                      \n\n📍 *3. Dirección:* Cl. 76 # 7B - 45, El Bosque, Barranquilla.\
-                      \n📌 https://maps.app.goo.gl/gteL23tYX32KUeb79\
-                      \n\n🌐 *Sitio oficial:* https://www.jw.org/es/\
-                      \n\n📢 *Recuerda:* Puedes volver a escribir *"menú"* para regresar.',
+                body=output_mesage["option_1"],
                 from_=TWILIO_PHONE,
                 to=from_number
             )
 
         elif message_body == '2':
             twilio_client.messages.create(
-                body='📌 *Aquí tienes los lugares de predicación para la semana:*\
-                      \n\n*Lunes:* 🏫 Salón del Reino – 6:50pm (pública)\
-                      \n*Martes:* 🏠 Esther Rua – 9:00am (cartas) | 🏠 Rosalba Serge – 4:30pm (casa en casa)\
-                      \n*Miercoles:* 🏠 Maria Herrera – 9:00am | 🏠 Vanessa Villa – 4:30pm\
-                      \n*Jueves:* ❌ No hay predicación (día de reunión)\
-                      \n*Viernes:* 🏫 Salón del Reino – 9:00am (cartas/llamadas) | 4:30pm (por definir)\
-                      \n*Sábado:* 🏠 Vanessa Villa – 9:00am\
-                      \n*Domingo:* 🏠 Por definir – 9:00am\
-                      \n\n📢 *Recuerda:* Escribe *"menú"* cuando quieras regresar al inicio.',
+                body=output_mesage["option_2"],
                 from_=TWILIO_PHONE,
                 to=from_number
             )
@@ -133,8 +95,9 @@ def webhook():
                 },
                 'datos': {}
             }
+            print("test: ",user_state)
             twilio_client.messages.create(
-                body='🗓️ Por favor, ingresa la *fecha* del informe en formato *AAAA-MM-DD*:',
+                body=output_mesage["option_3"],
                 from_=TWILIO_PHONE,
                 to=from_number
             )
--- app/state.py
+++ app/state.py
@@ -1,1 +1,60 @@
 user_state = {}
+
+output_mesage = {
+    "not_registered_message":'❌ 😔 *Lo siento, no estás registrado para usar este servicio.*\
+                    \n\nSi deseas registrarte, por favor contacta a tu super de grupo.\
+                    \n\nMientras tanto, quiero compartirte los horarios de reunión de la congregacion cordialidad:\
+                    \n\n🖥️ *1. Reunión entre semana | Jueves 7 pm*\
+                    \n📌 https://jworg.zoom.us/j/99106008401\
+                    \n*Código de acceso:* 1234.\
+                    \n\n🖥️ *2. Reunión fin de semana | Domingo 5 pm*\
+                    \n📌 https://jworg.zoom.us/j/91928746645\
+                    \n*Código de acceso:* 1234.\
+                    \n\n📍 *3. Dirección:* Cl. 76 # 7B - 45, El Bosque, Barranquilla.\
+                    \n📌 https://maps.app.goo.gl/gteL23tYX32KUeb79\
+                    \n\n🌐 *4. Sitio oficial:*\
+                    \n📌 https://www.jw.org/es/',
+
+    "welcome_message":("Hola {name} 😊 👋, por favor escribe *'menú'* para comenzar."),
+
+    "start_message":("*¡Ahora sí, mi querid@ herman@ {name}. 😊 ¿Cómo puedo ayudarte?*\n\n"
+                    "<<Escribe el # número de tu opción requerida>>:\n\n"
+                    "🕒 *1. Horas de reuniones.*\n"
+                    "🏠 *2. Lugares de Predicación.*\n"
+                    "📝 *3. Envío de informes.*\n"
+                    "📋 *4. Solicitudes.*\n"
+                    "👌 *5. Siervos ministeriales.*\n"
+                    "🫶 *6. Ancianos.*\n"
+                    "🚨 *7. Líneas de emergencia.*\n"
+                    ),
+    "option_1": '*Estos son los enlaces a la plataforma Zoom:*\
+                    \n\n🖥️ *1. Reunión entre semana | Jueves 7 pm*\
+                    \n📌 https://jworg.zoom.us/j/99106008401\
+                    \n*Código de acceso:* 1234.\
+                    \n\n🖥️ *2. Reunión fin de semana | Domingo 5 pm*\
+                    \n📌 https://jworg.zoom.us/j/91928746645\
+                    \n*Código de acceso:* 1234.\
+                    \n\n📍 *3. Dirección:* Cl. 76 # 7B - 45, El Bosque, Barranquilla.\
+                    \n📌 https://maps.app.goo.gl/gteL23tYX32KUeb79\
+                    \n\n🌐 *Sitio oficial:* https://www.jw.org/es/\
+                    \n\n📢 *Recuerda:* Puedes volver a escribir *"menú"* para regresar.',
+
+    "option_2": '📌 *Aquí tienes los lugares de predicación para la semana:*\
+                    \n\n*Lunes:* 🏫 Salón del Reino - 6:50pm (pública)\
+                    \n*Martes:* 🏠 Esther Rua - 9:00am (cartas) | 🏠 Rosalba Serge - 4:30pm (casa en casa)\
+                    \n*Miercoles:* 🏠 Maria Herrera - 9:00am | 🏠 Vanessa Villa - 4:30pm\
+                    \n*Jueves:* ❌ No hay predicación (día de reunión)\
+                    \n*Viernes:* 🏫 Salón del Reino - 9:00am (cartas/llamadas) | 4:30pm (por definir)\
+                    \n*Sábado:* 🏠 Vanessa Villa - 9:00am\
+                    \n*Domingo:* 🏠 Por definir - 9:00am\
+                    \n\n📢 *Recuerda:* Escribe *"menú"* cuando quieras regresar al inicio.',
+
+    "option_3": '🗓️ Por favor, ingresa seguido de comas (,)\
+                    \n\n*SI* / *NO.*\
+                    \n*Numero de estudios dirigidos.*\
+                    \n*(Opcional para precursores:)*: *R*/ regular, *A*/ auxiliar, *numero de horas*.\
+                    \n\n*Ejemplo de mensaje:*\
+                    \nPrecursores = Si,4,R,50.\
+                    \nPublicadores = Si,3 o simplemente No.'
+
+}
